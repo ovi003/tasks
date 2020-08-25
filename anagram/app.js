61450e9ef1918f42ecc0c3f3bb6d30498c9baf3e
Muazzem Hussen Chowdhury <mhcovi003@gmail.com>
2020-08-25 23:15:43 +0600

    Add design pattern and factory pattern

--- anagram/app.js
+++ anagram/app.js
@@ -5,8 +5,10 @@ const isAnagram = (word1, word2) => {
   const firstWord = getSortedString(word1);
   const secondWord = getSortedString(word2);
   firstWord === secondWord
-    ? console.log(`${word1} and ${word2} are Anagrams`)
-    : console.log(`${word1} and ${word2} are not Anagrams`);
+    ? console.log(`${word1} and ${word2} are anagrams`)
+    : console.log(`${word1} and ${word2} are not anagrams`);
 };
 
-isAnagram('Table', 'Bleat');
+isAnagram('bleat', 'table'); // Output: bleat and table are anagrams
+
+isAnagram('eat', 'tar'); // Output: eat and tar are not anagrams
